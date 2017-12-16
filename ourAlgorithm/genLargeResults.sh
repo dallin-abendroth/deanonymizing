ba1826fb384c83360150a5ff908783619f170c2d
Dallin Abendroth <dallin.abendroth@utah.edu>
2017-12-16 14:21:32 -0700

    take algorithm type as second argument for script

--- ourAlgorithm/genLargeResults.sh
+++ ourAlgorithm/genLargeResults.sh
@@ -1,12 +1,10 @@
 #/bin/bash
 
 CSV=$1 # use .csv as argument to this script
+ALG_TYPE=$2 
 TIMES_TO_RUN=100
 
-for alg in "simple" "directed"
+for k in {1..10}
 do
-  for k in {1..10}
-  do
-    python3 deanonymize.py $CSV $alg random $k $TIMES_TO_RUN
-  done
+  python3 deanonymize.py $CSV $ALG_TYPE random $k $TIMES_TO_RUN
 done
