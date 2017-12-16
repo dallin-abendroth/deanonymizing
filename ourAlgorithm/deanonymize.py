248998621171720df5a7d8559bbf30ccc9cba424
Dallin Abendroth <dallin.abendroth@utah.edu>
2017-12-16 01:36:24 -0700

    added checking up the tree for potential changes

--- ourAlgorithm/deanonymize.py
+++ ourAlgorithm/deanonymize.py
@@ -10,17 +10,24 @@ wght_i = 2
 
 class TreeNode(object):
     """Tree class made for quick convenience"""
-    def __init__(self, name, parent=None):
+    def __init__(self, name, match_in_h, parent=None):
         self.parent = parent
         self.name = name
+        self.match_in_h = match_in_h
         self.children = {}
-    def add(self, childname):
+    def add(self, childname, childmatch):
         if(childname not in self.children):
-            child = TreeNode(childname, self)
+            child = TreeNode(childname, childmatch, self)
             self.children[childname] = child
+            return True
+        return False
     def get(self, childname):
         if(childname in self.children):
             return self.children[childname]
+    def get_root(self):
+        if(self.parent==None):
+            return self
+        return self.parent.get_root()
     def leaf_nodes(self):
         if(not self.children):
             return [self]
@@ -28,6 +35,16 @@ class TreeNode(object):
         for child in self.children:
             leaf_nodes = leaf_nodes+self.children[child].leaf_nodes()
         return leaf_nodes
+    def path_to_root_h(self):
+        if(self.parent!=None):
+            return [self.match_in_h] + self.parent.path_to_root_h()
+        else:
+            return []
+    def path_to_root_all(self):
+        if(self.parent!=None):
+            return [(self.name, self.match_in_h)] + self.parent.path_to_root_all()
+        else:
+            return []
     def path_to_root(self):
         if(self.parent!=None):
             return [self.name] + self.parent.path_to_root()
@@ -86,43 +103,71 @@ def extract_matches(tree,k):
     for leaf in tree.leaf_nodes():
         path = leaf.path_to_root()
         if(len(path)==k):
-            matches.append(path)
-    return matches
+            matches.append(frozenset(path))
+    return set(matches)
 
 def tree_search(G,H,node,tree_search_rec_fn):
-    T = TreeNode('base')
+    T = TreeNode('base','base')
     for candidate in list(G.nodes):
         tree_search_rec_fn(G,H,T,candidate,node) 
     k = len(list(H.nodes))
     return extract_matches(T,k)
 
+def digraph_neighbors(G,node):
+    neighbors = []
+    neighbors.extend(G.predecessors)
+
 def tree_search_rec_directed(G,H,T,candidate_node,x):
     x_out_degree = H.nodes[x]['orig_out_degree'] 
     x_in_degree = H.nodes[x]['orig_in_degree'] 
+
+    real = ['3095', '4446', '1983'] # removed 35 and 3269
+
     if(candidate_node in T.path_to_root()): # potential nodes count once
         return
     if(G.out_degree[candidate_node]==x_out_degree # check out-degrees
             and G.in_degree[candidate_node]==x_in_degree): # check in-degrees
+        new_child = T.add(candidate_node,x)
+        if(not new_child):
+            return
         for x_nbr in H.successors(x): # check weights of out-edges
+            found_this_neighbor = False
             for cand_nbr in G.successors(candidate_node):
                 if(G[candidate_node][cand_nbr]['weight']==H[x][x_nbr]['weight']):
-                    T.add(candidate_node)
+                    if(candidate_node in real):
+                        print("Added ", candidate_node," here!")
                     tree_search_rec_directed(G,H,T.get(candidate_node),cand_nbr,x_nbr)
         for x_nbr in H.predecessors(x): # check weights of in-edges
             for cand_nbr in G.predecessors(candidate_node):
                 if(G[cand_nbr][candidate_node]['weight']==H[x_nbr][x]['weight']):
-                    T.add(candidate_node)
                     tree_search_rec_directed(G,H,T.get(candidate_node),cand_nbr,x_nbr)
 
 def tree_search_rec_simple(G,H,T,candidate_node,x):
-    x_degree = H.nodes[x]['orig_degree'] 
-    if(candidate_node in T.path_to_root()): # potential nodes count once
+    nodes_matched = T.path_to_root_h()
+    k = len(list(H.nodes))
+    if(candidate_node in T.path_to_root()
+            or x in nodes_matched): # potential nodes count once
         return
+    x_degree = H.nodes[x]['orig_degree'] 
     if(G.degree[candidate_node]==x_degree): # check degrees
+        new_child = T.add(candidate_node,x)
+        T = T.get(candidate_node)
+        nodes_matched = T.path_to_root_h()
+        if(not new_child 
+                or len(nodes_matched) == k):
+            return
         for x_nbr in H.neighbors(x): 
+            if(x_nbr in nodes_matched):
+                continue
             for cand_nbr in G.neighbors(candidate_node):
-                T.add(candidate_node)
-                tree_search_rec_simple(G,H,T.get(candidate_node),cand_nbr,x_nbr)
+                tree_search_rec_simple(G,H,T,cand_nbr,x_nbr)
+        for parent in T.parent.path_to_root_all(): # check for paths only accessible to nodes previously visited
+            # this check will probably add a lot of time :(
+            for x_parent_nbr in H.neighbors(parent[1]):
+                if(x_parent_nbr in nodes_matched):
+                    continue
+                for cand_parent_nbr in G.neighbors(parent[0]):
+                    tree_search_rec_simple(G,H,T,cand_parent_nbr,x_parent_nbr)
 
 def setup_H_subgraph_directed(G, subG):
     nx.set_node_attributes(subG, 'orig_out_degree', 0)
@@ -147,6 +192,15 @@ def deanonymize_weighted_directed(G,k,given_coalition):
     matches = tree_search(G, subG, initial_attacker, tree_search_rec_directed)
     if(len(matches)==0):
         print("!!! zero matches found, this should be impossible. Coalition below !!!")
+        print(' '.join(coalition))
+        print(subG.edges(coalition))
+        print(subG.edges(coalition))
+    matched_self = False
+    for match in matches:
+        if set(coalition)==set(match):
+            matched_self = True
+    if(matched_self):
+        print("!!! original coalition not found, this should be impossible. Coalition below !!!")
         print(' '.join(coalition))
     if(len(matches)!=1):
         return 0
@@ -215,7 +269,7 @@ if __name__ == "__main__":
         for i in range(0,times_to_run):
             start = dt.datetime.now()
             results.append(deanonymize_simple(G,k,given_coalition))
-            timings.append(dt.datetime.now()-start)
+            timings.append((dt.datetime.now()-start).microseconds / 1000)
         avg = sum(results) / float(len(results))
         avgtime = sum(timings) / float(len(timings))
         prob_success = (len(results) - results.count(0)) / float(len(results))
@@ -240,7 +294,7 @@ if __name__ == "__main__":
         for i in range(0,times_to_run):
             start = dt.datetime.now()
             results.append(deanonymize_weighted_directed(G,k,given_coalition))
-            timings.append(dt.datetime.now()-start)
+            timings.append((dt.datetime.now()-start).microseconds / 1000)
         avg = sum(results) / float(len(results))
         avgtime = sum(timings) / float(len(timings))
         prob_success = (len(results) - results.count(0)) / float(len(results))
