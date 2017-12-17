91f4b17e41c22ccc465cddbcdeaf08fcf0e3e476
Dallin Abendroth <dallin.abendroth@utah.edu>
2017-12-16 18:37:32 -0700

    combine duplicated code

--- ourAlgorithm/deanonymize.py
+++ ourAlgorithm/deanonymize.py
@@ -81,10 +81,13 @@ def subgraph_neighbors(G,nodes):
     return set(neighbors)
 
 def coalition_neighbors(G,attacker):
+    return neighbors_all(G,attacker)
+
+def neighbors_all(G,node):
     neighbors = []
-    neighbors.extend(list(G.neighbors(attacker)))
+    neighbors.extend(list(G.neighbors(node)))
     if(nx.is_directed(G)):
-        neighbors.extend(list(G.predecessors(attacker)))
+        neighbors.extend(list(G.predecessors(node)))
     return set(neighbors)
 
 def make_coalition(G,attacker,k):
@@ -117,49 +120,49 @@ def extract_matches(tree,k):
             matches.append(frozenset(path))
     return set(matches)
 
-def tree_search_rec_directed(G,H,T,candidate_node,x):
+def edge_match(G,H,x_pair,cand_pair):
+    if(not nx.is_directed(G)): return True
+
+    H_has_edge = H.has_edge(x_pair)
+    H_has_edge_r = H.has_edge(reversed(x_pair))
+
+    G_has_edge = G.has_edge(cand_pair)
+    G_has_edge_r = G.has_edge(reversed(cand_pair))
+
+    if(H_has_edge and G_has_edge):
+        if(H.get_edge_data(x_pair)['weight'] != G.get_edge_data(cand_pair)['weight']):
+            return false
+    elif(not (not H_has_edge and not G_has_edge)):
+        return False
+
+    if(H_has_edge_r and G_has_edge_r):
+        if(H.get_edge_data(reversed(x_pair))['weight'] != G.get_edge_data(reversed(cand_pair))['weight']):
+            return false
+    elif(not (not H_has_edge_r and not G_has_edge_r)):
+        return False
+
+    return True
+
+def tree_search_rec(G,H,T,candidate_node,x):
     global multiple_matches_failure
     if(multiple_matches_failure): return
-    for x_nbr in H.successors(x): # check weights of out-edges
+    for x_nbr in neighbors_all(H,x): 
         if(x_nbr in T.H_nodes_matched()): continue
-        for cand_nbr in G.successors(candidate_node):
+        for cand_nbr in neighbors_all(G,candidate_node):
             if(multiple_matches_failure): return
             if(cand_nbr in T.G_nodes_matched()): continue
-            if(G[candidate_node][cand_nbr]['weight']==H[x][x_nbr]['weight']
-                    and degree_match(G,H,x_nbr,cand_nbr)):
-                newT = T.add(cand_nbr, x_nbr)
-                if(check_mult_matches(newT, k, newT.H_nodes_matched())): return
-                tree_search_rec_simple(G,H,newT,candidate_node,x)
-    for x_nbr in H.predecessors(x): # check weights of in-edges
-        if(x_nbr in T.H_nodes_matched()): continue
-        for cand_nbr in G.predecessors(candidate_node):
-            if(multiple_matches_failure): return
-            if(cand_nbr in T.G_nodes_matched()): continue
-            if(G[cand_nbr][candidate_node]['weight']==H[x_nbr][x]['weight']
-                    and degree_match(G,H,x_nbr,cand_nbr)):
+            if(degree_match(G,H,x_nbr,cand_nbr) 
+                    and edge_match(G,H,(x,x_nbr),(candidate_node, cand_nbr))):
                 newT = T.add(cand_nbr, x_nbr)
                 if(check_mult_matches(newT, k, newT.H_nodes_matched())): return
                 tree_search_rec_simple(G,H,newT,candidate_node,x)
 
-def tree_search_rec_simple(G,H,T,candidate_node,x):
-    global multiple_matches_failure
-    if(multiple_matches_failure): return
-    for x_nbr in H.neighbors(x): 
-        if(x_nbr in T.H_nodes_matched()): continue
-        for cand_nbr in G.neighbors(candidate_node):
-            if(multiple_matches_failure): return
-            if(cand_nbr in T.G_nodes_matched()): continue
-            if(degree_match(G,H,x_nbr,cand_nbr)):
-                newT = T.add(cand_nbr, x_nbr)
-                if(check_mult_matches(newT, k, newT.H_nodes_matched())): return
-                tree_search_rec_simple(G,H,newT,candidate_node,x)
-
-def tree_search(G,H,attacker, tree_search_rec_fn):
+def tree_search(G,H,attacker):
     T = TreeNode('base','base')
     for candidate in list(G.nodes):
         if(degree_match(G,H,attacker,candidate)):
             T_cand = T.add(candidate,attacker)
-            tree_search_rec_fn(G,H,T_cand,candidate,attacker) 
+            tree_search_rec(G,H,T_cand,candidate,attacker) 
     k = len(list(H.nodes))
     return extract_matches(T,k)
 
@@ -195,33 +198,7 @@ def setup_H_subgraph(G, subG):
         subG.nodes[node]['orig_degree'] = G.degree[node]
         if(directed): subG.nodes[node]['orig_in_degree'] = G.in_degree[node]
 
-def deanonymize_weighted_directed(G,k,given_coalition):
-    global multiple_matches_failure
-    initial_attacker = random.choice(list(G.nodes))
-    coalition = make_coalition(G, initial_attacker, k)
-    if(len(given_coalition)>0):
-        initial_attacker = given_coalition[0]
-        coalition = given_coalition
-    subG = nx.subgraph(G, coalition)
-    setup_H_subgraph(G, subG)
-    matches = tree_search(G, subG, initial_attacker, tree_search_rec_directed)
-    if(len(matches)==0):
-        print("!!! zero matches found, this should be impossible. Coalition below !!!")
-        print(' '.join(coalition))
-        print(subG.edges(coalition))
-        print(subG.edges(coalition))
-    matched_self = False
-    for match in matches:
-        if set(coalition)==set(match):
-            matched_self = True
-    if(not matched_self and not multiple_matches_failure):
-        print("!!! original coalition not found, this should be impossible. Coalition below !!!")
-        print(' '.join(coalition))
-    if(len(matches)!=1): # cannot deanonymize
-        return 0
-    return len(subgraph_neighbors(G, coalition))
-
-def deanonymize_simple(G,k,given_coalition):
+def deanonymize(G,k,given_coalition):
     global multiple_matches_failure
     initial_attacker = random.choice(list(G.nodes))
     coalition = make_coalition(G, initial_attacker, k)
@@ -230,7 +207,7 @@ def deanonymize_simple(G,k,given_coalition):
         coalition = given_coalition
     subG = nx.subgraph(G,coalition)
     setup_H_subgraph(G, subG)
-    matches = tree_search(G, subG, initial_attacker, tree_search_rec_simple)
+    matches = tree_search(G, subG, initial_attacker)
     if(len(matches)==0):
         print("!!! zero matches found, this should be impossible. Coalition below !!!")
         print(' '.join(coalition))
@@ -289,47 +266,25 @@ if __name__ == "__main__":
         except:
             print_usage()
 
-    if(sys.argv[2]=='simple'):
-        print("--------------------------------")
-        print("Running simple algorithm ",times_to_run," time(s) on dataset: ",sys.argv[1])
+    if(sys.argv[2]=='simple' or sys.argv[2]=='directed'):
+        if(sys.argv[2]=='simple'):
+            print("--------------------------------")
+            print("Running simple algorithm ",times_to_run," time(s) on dataset: ",sys.argv[1])
+            G = get_simple_graph_from_csv(sys.argv[1])
+        elif(sys.argv[2]=='directed'):
+            print("--------------------------------")
+            print("Running weighted, directed algorithm ",times_to_run," time(s) on dataset: ",sys.argv[1])
+            G = get_weighted_directed_graph_from_csv(sys.argv[1])
         if(len(given_coalition)>0):
             print("Using given coalition: ",given_coalition)
         print("k = ",k)
-        G = get_simple_graph_from_csv(sys.argv[1])
         results = []
         timings = []
         for i in range(0,times_to_run):
             multiple_matches_failure = False
             matches_so_far = 0
             start = dt.datetime.now()
-            results.append(deanonymize_simple(G,k,given_coalition))
-            timings.append((dt.datetime.now()-start).microseconds / 1000)
-        avg = sum(results) / float(len(results))
-        avgtime = sum(timings) / float(len(timings))
-        prob_success = (len(results) - results.count(0)) / float(len(results))
-        if(times_to_run>1):
-            print("avg = ",avg)
-            print("probability of success = ",prob_success)
-            print("avg time = ",avgtime)
-        else:
-            print("Deanonymized nodes: ",results[0])
-        print("--------------------------------")
-        sys.exit(0)
-    
-    if(sys.argv[2]=='directed'):
-        print("--------------------------------")
-        print("Running weighted, directed algorithm ",times_to_run," time(s) on dataset: ",sys.argv[1])
-        if(len(given_coalition)>0):
-            print("Using given coalition: ",given_coalition)
-        print("k = ",k)
-        G = get_weighted_directed_graph_from_csv(sys.argv[1])
-        results = []
-        timings = []
-        for i in range(0,times_to_run):
-            multiple_matches_failure = False
-            matches_so_far = 0
-            start = dt.datetime.now()
-            results.append(deanonymize_weighted_directed(G,k,given_coalition))
+            results.append(deanonymize(G,k,given_coalition))
             timings.append((dt.datetime.now()-start).microseconds / 1000)
         avg = sum(results) / float(len(results))
         avgtime = sum(timings) / float(len(timings))
